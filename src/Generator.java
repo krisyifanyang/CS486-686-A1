208d5d5f2af02fa84c61e7b274a47e9cc8ccdfcd
Haochen Ji <hcji@uwaterloo.ca>
2016-10-01 12:42:04 -0400

    Start

---
+++ src/Generator.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jamesji on 2016-10-01.
+ */
+public class Generator {
+    public String generate(String startingWord, String[] sentenceSpec, String graph)
+    {
+        
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
