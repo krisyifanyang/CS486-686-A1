6282d11c0da98ee80b3f40b10cd981215bc87631
krisyang <krisyang>
2016-10-04 06:50:02 -0400

    bfs dfs

--- src/Generator.java
+++ src/Generator.java
@@ -1,8 +1,92 @@
 public class Generator {
-    public String generate(String startingWord, String[] sentenceSpec, String graph)
+    public static String generate(String startingWord, String[] sentenceSpec, Corpus graph)
     {
 
+
+        string searchWord= startingWord;
+
+
+        graph.getWord2Prob(searchWord)
+
+
         return "";
+    }
+
+
+
+    public static void dfs(String startingWord, String[] sentenceSpec, Corpus graph, int counter, ArrayList<String> curRes, ArrayList<String> finalRes double curProb, double[] finalProb){
+
+        // counter fisrt is 0
+        // end of a successful search, compare curProb with finalProb, if curProb < finalProb, update finalRes to curRes
+        if (counter + 1 = sentenceSpec.length){
+
+            if(curProb > finalProb[0]){
+                finalRes=curRes;
+                finalProb[0] = curProb;
+            }
+            return;
+        }
+
+        String[][] subnodes = graph.getWord2Prob(startingWord, sentenceSpec[counter], sentenceSpec[counter+1]);
+        if (subnodes.length ==0) {
+            return;
+        }
+
+        for(int i =0; i< subnodes.length; i++){
+
+            curRes.add(subnodes[i][0]);
+            dfs( subnodes[i][0], sentenceSpec, graph, counter+1,  curRes, finalRes , curProb*Double.parseDouble(subnodes[i][1]), finalProb);
+            curRes.remove(curRes.size()-1);
+
+        }
+        return;
+    }
+
+
+    public static void bfs(String startingWord, String[] sentenceSpec, Corpus graph){
+
+        String[][] subnodes = graph.getWord2Prob(startingWord, sentenceSpec[counter], sentenceSpec[counter+1]);
+        if (subnodes.length ==0) {
+            return;
+        }
+
+        for(int i =0; i< subnodes.length; i++){
+
+        
+
+        Queue<node> queue = new LinkedList<>();
+        ArrayList<node> explored = new ArrayList<>();
+        queue.add(this.startNode);
+        explored.add(startNode);
+
+        while(!queue.isEmpty()){
+            Node current = queue.remove();
+
+       
+                if(current.getChildren().isEmpty())
+                    return false;
+                else
+                    queue.addAll(current.getChildren());
+           
+            explored.add(current);
+        }
+
+        return false;
+
+    }
+
+
+
+
+
+    }
+
+
+    public static void astar(String startingWord, String[] sentenceSpec, Corpus graph){
+
+
+
+        
     }
 
     public static void main(String[] args) {
